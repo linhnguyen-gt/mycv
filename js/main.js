86c6287810edcbdfac709210e2829a2ed78fc84e
linhnguyengas <linhgaster@gmail.com>
2019-08-05 23:07:45 +0700

    Initial commit

--- js/main.js
+++ js/main.js
@@ -3,8 +3,30 @@ function darkLightMode(){
     var current = body.className;
     body.className = current == 'dark-mode' ? 'light-mode' : 'dark-mode';
 
-
     var element = document.getElementsByClassName('inner');
     var click = element[0];
     click.classList.toggle('active');
 }
+var get = document.querySelector('i');
+function backHead(){
+    var height = self.pageYOffset;
+    var set = height;
+    var run = setInterval(function(){
+        height = height - 0.05*set;
+        window.scrollTo(0,height);
+        if(height <= 0 ){
+            clearInterval(run);
+        }
+    });
+}
+function load(){
+    var get = document.getElementById('backHead')
+    if(window.scrollY > 300){
+        get.classList.add('actived');
+        console.log('hihi')
+    }else{
+        get.classList.remove('actived');
+    }
+}
+window.onscroll = function(){load()};
+
